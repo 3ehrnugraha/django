b3b03702c3888c1578019631e5090bf3390db021
3ehrnugraha <nugraha@3ecpa.com.sg>
2024-07-09 18:44:21 +0700

    Add QuerySet filter

--- learndjango/division/views.py
+++ learndjango/division/views.py
@@ -1,7 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Division
+from employees.models import Employee
 from .serializers import DivisionSerializer
 
 class DivisionViewSet(viewsets.ModelViewSet):
     queryset = Division.objects.all()
     serializer_class = DivisionSerializer
+
+    def get_queryset(self):
+        queryset = Division.objects.all()
+        name = self.request.query_params.get('name', None)
+        description = self.request.query_params.get('description', None)
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if description:
+            queryset = queryset.filter(description__icontains=description)
+
+        return queryset
